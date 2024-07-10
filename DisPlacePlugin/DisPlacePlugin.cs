3d825d2fc345e8562ce3ff30cef5b2f899e17492
Drakansoul <redlineblue@gmail.com>
2024-07-10 13:43:29 -0700

    fix item placement

--- DisPlacePlugin/DisPlacePlugin.cs
+++ DisPlacePlugin/DisPlacePlugin.cs
@@ -30,6 +30,9 @@ namespace DisPlacePlugin
         public delegate void SelectItemDelegate(IntPtr housingStruct, IntPtr item);
         private static HookWrapper<SelectItemDelegate> SelectItemHook;
 
+        public delegate void PlaceItemDelegate(IntPtr housingStruct, IntPtr item);
+        private static HookWrapper<PlaceItemDelegate> PlaceItemHook;
+
         public static bool CurrentlyPlacingItems = false;
 
         public static bool ApplyChange = false;
@@ -85,7 +88,9 @@ namespace DisPlacePlugin
             IsSaveLayoutHook = HookManager.Hook<UpdateLayoutDelegate>("40 53 48 83 ec 20 48 8b d9 48 8b 0d ?? ?? ?? ?? e8 ?? ?? ?? ?? 33 d2 48 8b c8 e8 ?? ?? ?? ?? 84 c0 75 ?? 38 83 ?? 01 00 00", IsSaveLayoutDetour);
 
             SelectItemHook = HookManager.Hook<SelectItemDelegate>("48 85 D2 0F 84 49 09 00 00 53 41 56 48 83 EC 48 48 89 6C 24 60 48 8B DA 48 89 74 24 70 4C 8B F1", SelectItemDetour);
-            
+
+            PlaceItemHook = HookManager.Hook<PlaceItemDelegate>("48 89 5C 24 10 48 89 74  24 18 57 48 83 EC 20 4c 8B 41 18 33 FF 0F B6 F2", PlaceItemDetour);
+
             UpdateYardObjHook = HookManager.Hook<UpdateYardDelegate>("48 89 74 24 18 57 48 83 ec 20 b8 dc 02 00 00 0f b7 f2 ??", UpdateYardObj);
 
             GetGameObjectHook = HookManager.Hook<GetObjectDelegate>("48 89 5c 24 08 48 89 74 24 10 57 48 83 ec 20 0f b7 f2 33 db 0f 1f 40 00 0f 1f 84 00 00 00 00 00", GetGameObject);
@@ -132,6 +137,7 @@ namespace DisPlacePlugin
         
         unsafe static public void SelectItemDetour(IntPtr housing, IntPtr item)
         {
+            DalamudApi.PluginLog.Debug(string.Format("selecting item {0}",item.ToString()));
             SelectItemHook.Original(housing, item);
         }
 
@@ -139,6 +145,18 @@ namespace DisPlacePlugin
         unsafe static public void SelectItem(IntPtr item)
         {
             SelectItemDetour((IntPtr)Memory.Instance.HousingStructure, item);
+        }
+
+        unsafe static public void PlaceItemDetour(IntPtr housing, IntPtr item)
+        {
+            DalamudApi.PluginLog.Debug(string.Format("placing item {0}",(housing+24).ToString()));
+            PlaceItemHook.Original(housing, item);
+        }
+
+
+        unsafe static public void PlaceItem(IntPtr item)
+        {
+            PlaceItemDetour((IntPtr)Memory.Instance.HousingStructure, item);
         }
         
 
@@ -233,6 +251,8 @@ namespace DisPlacePlugin
             }
             MemInstance.WritePosition(position);
             MemInstance.WriteRotation(rotation);
+
+            PlaceItem(nint.Zero);
 
             rowItem.CorrectLocation = true;
             rowItem.CorrectRotation = true;
